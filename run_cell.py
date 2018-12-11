129648598b726d0dfa6db7767639173e220a32f6
Steve Charczynski <stevecharczynski@Steves-MacBook-Pro.local>
2018-12-11 13:01:37 -0500

    change to const bounds

--- run_cell.py
+++ run_cell.py
@@ -23,7 +23,7 @@ mean_bounds = (
     (time_info.region_high + mean_delta))
 
 bounds = ((0.001, 1 / n), mean_bounds, (10, 1000), (10**-10, 1 / n))
-bounds_c = (10**-10, 0.99)
+bounds_c = [(10**-10, 0.99)]
 pipeline = AnalysisPipeline(cell_range, data_processor, ["Const", "Time"], 0)
 pipeline.set_model_bounds("Time", bounds)
 pipeline.set_model_bounds("Const", bounds_c)
